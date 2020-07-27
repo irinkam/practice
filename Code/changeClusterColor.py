35f793d93b73c51847955bddf56be06506e705a2
Irina <irinka9411@gmail.com>
2020-07-28 04:10:29 +0700

    changed color of large clusters

--- Code/changeClusterColor.py
+++ Code/changeClusterColor.py
@@ -5,10 +5,10 @@ import chardet
 import os
 import codecs
 
-filename = 'map3.html'
+filename = 'map1.html'
 filepath = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "\\" + "HtmlMaps" + "\\" + filename
 
-#File opening
+# File opening
 bytes = min(32, os.path.getsize(filepath))
 raw = open(filepath, 'rb').read(bytes)
 
@@ -20,13 +20,13 @@ else:
 infile = io.open(filepath, 'r', encoding=encoding)
 data = infile.read()
 infile.close()
-#File closed
+# File closed
 
-#Html parsing
+# Html parsing
 soup = Soup(data, features="html.parser")
 head = soup.find('head')
 
-#Adding style tag (hint: some magic with cluster colors happens here)
+# Adding style tag (hint: some magic with cluster colors happens here)
 inside_color = 'rgba(119,136,153, 0.6)'
 outside_color = 'rgba(192,192,192, 0.6)'
 style = soup.new_tag('style')
@@ -37,18 +37,24 @@ style.append(f"""
 .marker-cluster-medium {{
     background-color: {outside_color};
 }}
+.marker-cluster-large {{
+    background-color: {outside_color};
+}}
 .marker-cluster-small div {{
     background-color: {inside_color};
 }}
 .marker-cluster-medium div {{
     background-color: {inside_color};
 }}
+.marker-cluster-large div {{
+    background-color: {inside_color};
+}}
 """)
 head.append(style)
 
-#print(head)
+# print(head)
 
-#Creation of 'generated' folder
+# Creation of 'generated' folder
 generated_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "\\" + 'HtmlMaps\generated'
 print(generated_path)
 
@@ -56,17 +62,16 @@ if not os.path.exists(generated_path):
     try:
         os.mkdir(generated_path)
     except OSError:
-        print ("Creation of the directory '%s' failed" % generated_path)
+        print("Creation of the directory '%s' failed" % generated_path)
     else:
-        print ("Successfully created the directory '%s'" % generated_path)
+        print("Successfully created the directory '%s'" % generated_path)
 else:
-    print ("Directory '%s' already exists" % generated_path)
+    print("Directory '%s' already exists" % generated_path)
 
-
-#Writing new file with template name "_{filename}.html" into "generated" directory
+# Writing new file with template name "_{filename}.html" into "generated" directory
 f = open(generated_path + "/" + "_" + filename, "w")
 f.write(str(soup))
 f.close()
 
-#print(style)
-#print(head)
+# print(style)
+# print(head)
